850dfc2c0b6db5da48b1508894af3b3f97d8f181
Owner-PC\Owner <dayg502@gmail.com>
2017-05-27 21:16:35 +0900

    QtShape getKey() 추가

--- FinalQt/FinalQt/QtShape.cpp
+++ FinalQt/FinalQt/QtShape.cpp
@@ -19,6 +19,11 @@ CPoint QtShape::relativeToAbsolute(CPoint lu, CPoint rd, CPoint relPos) {
 	return diff;
 }
 
+int QtShape::getKey()
+{
+	return key;
+}
+
 int QtShape::nearPoint(CPoint pos1, CPoint pos2) {
 	int dx = abs(pos1.x - pos2.x);
 	int dy = abs(pos1.y - pos2.y);
@@ -26,4 +31,8 @@ int QtShape::nearPoint(CPoint pos1, CPoint pos2) {
 	if (abs(pos1.x - pos2.x) <= 2 && abs(pos1.y - pos2.y) <= 2) //+-2pixel 이내에 있으면
 		return dx + dy; // 0~4, 작을수록 근접
 	else return -1;
+}
+
+bool QtShape::inRect(CPoint lu, CPoint rd, CPoint pos) {
+	return (lu.x <= pos.x && pos.x <= rd.x && lu.y <= pos.y && pos.y <= rd.y);
 }
